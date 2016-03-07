8fb4455bdd3472c1ebe9c912803a770e61fc1307
47767573t <Git1234>
2016-03-07 18:04:28 +0100

    probando tilemap

--- UFO2D-89f350a789a564829b98033fd15ee4fb7dbec077/main.js
+++ UFO2D-89f350a789a564829b98033fd15ee4fb7dbec077/main.js
@@ -17,17 +17,26 @@ var mainState = (function (_super) {
         _super.prototype.preload.call(this);
         this.load.image('ufo', 'assets/UFO_low.png');
         this.load.image('pickup', 'assets/Pickup_low.png');
-        this.load.image('background', 'assets/Background_low.png');
+        //Background Tilled
+        this.game.load.tilemap('bgTilledMap', 'assets/bgTilled.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('bgLow', 'assets/Background_low.png');
+        /*//piezas de background orden
+        this.load.image('bgArribaIzquierda', 'assets/Background-0-0.png');
+        this.load.image('bgArriba', 'assets/Background-0-1.png');
+        this.load.image('bgArribaDerecha', 'assets/Background-0-2.png');
+        this.load.image('bgIzquierda', 'assets/Background-1-0.png');
+        this.load.image('bgCentro', 'assets/Background-1-1.png');
+        this.load.image('bgDerecha', 'assets/Background-1-2.png');
+        this.load.image('bgAbajoIzquierda', 'assets/Background-2-0.png');
+        this.load.image('bgAbajo', 'assets/Background-2-1.png');
+        this.load.image('bgAbajoDerecha', 'assets/Background-2-2.png');*/
+        //this.load.image('background', 'assets/Background_low.png');
         //this.physics.startSystem(Phaser.Physics.ARCADE);
     };
     mainState.prototype.create = function () {
         _super.prototype.create.call(this);
-        var background;
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
+        this.createWalls();
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
-        this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
         this.ufo.anchor.setTo(0.5, 0.5);
         this.physics.enable(this.ufo);
         this.cursor = this.input.keyboard.createCursorKeys();
@@ -35,6 +44,13 @@ var mainState = (function (_super) {
         this.ufo.body.drag.setTo(this.UFO_FRICTION, this.UFO_FRICTION);
         this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.setTo(1);
+    };
+    mainState.prototype.createWalls = function () {
+        this.map = this.game.add.tilemap('bgTilledMap');
+        this.map.addTilesetImage('Background_low', 'bgLow');
+        this.walls = this.map.createLayer("Tile Layer 1");
+        var background = this.map.createLayer('Tile Layer 2');
+        this.map.setCollisionBetween(1, 100, true, 'walls');
     };
     mainState.prototype.update = function () {
         _super.prototype.update.call(this);
@@ -45,11 +61,15 @@ var mainState = (function (_super) {
         else if (this.cursor.right.isDown) {
             this.ufo.body.acceleration.x = this.UFO_ACCELERATION;
         }
-        if (this.cursor.up.isDown) {
+        else if (this.cursor.up.isDown) {
             this.ufo.body.acceleration.y = -this.UFO_ACCELERATION;
         }
         else if (this.cursor.down.isDown) {
             this.ufo.body.acceleration.y = this.UFO_ACCELERATION;
+        }
+        else {
+            this.ufo.body.acceleration.y = 0;
+            this.ufo.body.acceleration.x = 0;
         }
     };
     return mainState;
--- UFO2D-89f350a789a564829b98033fd15ee4fb7dbec077/main.ts
+++ UFO2D-89f350a789a564829b98033fd15ee4fb7dbec077/main.ts
@@ -4,11 +4,15 @@ class mainState extends Phaser.State {
     game: Phaser.Game;
     private ufo:Phaser.Sprite;
     private cursor:Phaser.CursorKeys;
+    private walls:Phaser.TilemapLayer;
 
     //var de movimiento
     private UFO_MAX_SPEED = 200;
     private UFO_FRICTION = 30;
     private UFO_ACCELERATION = 180;
+
+    //Background
+    private map:Phaser.Tilemap;
 
 
 
@@ -17,20 +21,34 @@ class mainState extends Phaser.State {
 
         this.load.image('ufo', 'assets/UFO_low.png');
         this.load.image('pickup', 'assets/Pickup_low.png');
-        this.load.image('background', 'assets/Background_low.png');
+
+        //Background Tilled
+        this.game.load.tilemap('bgTilledMap', 'assets/bgTilled.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('bgLow', 'assets/Background_low.png');
+
+        /*//piezas de background orden
+        this.load.image('bgArribaIzquierda', 'assets/Background-0-0.png');
+        this.load.image('bgArriba', 'assets/Background-0-1.png');
+        this.load.image('bgArribaDerecha', 'assets/Background-0-2.png');
+        this.load.image('bgIzquierda', 'assets/Background-1-0.png');
+        this.load.image('bgCentro', 'assets/Background-1-1.png');
+        this.load.image('bgDerecha', 'assets/Background-1-2.png');
+        this.load.image('bgAbajoIzquierda', 'assets/Background-2-0.png');
+        this.load.image('bgAbajo', 'assets/Background-2-1.png');
+        this.load.image('bgAbajoDerecha', 'assets/Background-2-2.png');*/
+
+
+
+        //this.load.image('background', 'assets/Background_low.png');
         //this.physics.startSystem(Phaser.Physics.ARCADE);
     }
 
     create():void {
         super.create();
-        var background;
+        this.createWalls();
 
-        background = this.add.sprite(0, 0, 'background');
-        var scale = this.world.height / background.height;
-        background.scale.setTo(scale, scale);
 
         this.ufo = this.add.sprite(this.world.centerX, this.world.centerY, 'ufo');
-        this.ufo.scale.setTo(scale - 0.05, scale - 0.05);
         this.ufo.anchor.setTo(0.5, 0.5);
 
         this.physics.enable(this.ufo);
@@ -40,8 +58,18 @@ class mainState extends Phaser.State {
         this.ufo.body.drag.setTo(this.UFO_FRICTION, this.UFO_FRICTION);
         this.ufo.body.collideWorldBounds = true;
         this.ufo.body.bounce.setTo(1);
+    }
+
+    private createWalls() {
+        this.map = this.game.add.tilemap('bgTilledMap');
+        this.map.addTilesetImage('Background_low', 'bgLow');
 
 
+        this.walls = this.map.createLayer("Tile Layer 1");
+
+
+        var background = this.map.createLayer('Tile Layer 2');
+        this.map.setCollisionBetween(1, 100, true, 'walls');
     }
 
     update():void {
@@ -53,13 +81,15 @@ class mainState extends Phaser.State {
 
             } else if (this.cursor.right.isDown) {
                 this.ufo.body.acceleration.x =this.UFO_ACCELERATION;
-            }
 
-            if (this.cursor.up.isDown) {
+            } else if (this.cursor.up.isDown) {
                 this.ufo.body.acceleration.y =-this.UFO_ACCELERATION;
 
             } else if (this.cursor.down.isDown) {
                 this.ufo.body.acceleration.y =this.UFO_ACCELERATION;
+            } else {
+                this.ufo.body.acceleration.y =0;
+                this.ufo.body.acceleration.x =0;
             }
 
     }
